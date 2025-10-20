2641269e66fc6c18bf8f7e2dff96350e9fe68620
Michael Sena <michaelsena@microsoft.com>
2025-10-20 15:55:08 +1100

    add an acr

--- 01-create-aks.sh
+++ 01-create-aks.sh
@@ -4,13 +4,15 @@ set -eou pipefail
 
 az group create -n "$RG" -l "$LOC"
 
+az acr create --admin-enabled true --sku standard -g "$RG" -l "$LOC" -n "$ACR_NAME"
+
 az aks create \
   -g "$RG" -n "$CLUSTER" \
   --location "$LOC" \
   --enable-managed-identity \
   --node-count 1 \
   --node-vm-size Standard_D4s_v3 \
-  --no-ssh-key
+  --attach-acr $ACR_NAME
 
 # Verify
 az aks show -g "$RG" -n "$CLUSTER" --query "provisioningState" -o tsv | grep -x Succeeded
