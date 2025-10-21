3a556876020dc94c87a115532e57218a0a26ef5b
Michael Sena <michaelsena@microsoft.com>
2025-10-21 13:32:45 +1100

    Fix small stuff

--- 01-create-aks.sh
+++ 01-create-aks.sh
@@ -6,7 +6,7 @@ log Creating the resource group...
 az group create -n "$RESOURCE_GROUP" -l "$REGION"
 
 log Creating the ACR...
-az acr create --admin-enabled true --sku standard -g "$RESOURCE_GROUP" -l "$REGION" -n "$ACR_NAME"
+az acr create --admin-enabled true --sku standard -g "$RESOURCE_GROUP" -l "$REGION" -n "$ACR_NAME" --only-show-errors
 
 log Creating the cluster...
 az aks create \
@@ -15,7 +15,8 @@ az aks create \
   --enable-managed-identity \
   --node-count 1 \
   --node-vm-size Standard_D4s_v3 \
-  --attach-acr $ACR_NAME
+  --attach-acr $ACR_NAME \
+  --only-show-errors
 
 # Setup environment.
 ./local-env.sh
