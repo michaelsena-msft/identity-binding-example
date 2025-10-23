c5678ad51ff50c88eb3d17a687ea9a252625f2c5
Michael Sena <michaelsena@microsoft.com>
2025-10-23 11:55:36 +1100

    Further fixes to step 05

--- 05-install-ingress.sh
+++ 05-install-ingress.sh
@@ -4,9 +4,18 @@ set -eou pipefail
 
 ./operations/configure-${MODE}.sh
 
+# Get the controller location
+if [ "${MODE}" = "ingress-nginx" ]; then
+  NAMESPACE=ingress-nginx
+  CONTROLLER=ingress-nginx-controller
+elif [ "${MODE}" == "nginx-ingress" ]; then
+  NAMESPACE=default
+  CONTROLLER=nginx-ingress-release-controller
+fi
+
 log Waiting for external IP
 for i in $(seq 1 60); do
-  EXTERNAL_IP="$(k -n default get svc/nginx-ingress-release-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)"
+  EXTERNAL_IP="$(k -n ${NAMESPACE} get svc/${CONTROLLER} -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)"
   [ -n "${EXTERNAL_IP:-}" ] && break
   sleep 5
 done
--- 07-replace-image.sh
+++ 07-replace-image.sh
@@ -2,11 +2,11 @@
 set -eou pipefail
 [ -f ./.env ] && . ./.env || . ../.env
 
-if [ ${MODE} == "ingress-nginx" ]; then
+if [ "${MODE}" == "ingress-nginx" ]; then
     DEFAULT_LABEL=$(grep -o '^-\W\+image:.*$' ./patches/controller-image.patch | awk '{print $3}')
     ALT_REPOSITORY=$(echo ${DEFAULT_LABEL} | sed -e "s/registry.k8s.io/${ACR_FQDN}/g; s/@.\+//g")
     ALT_VERSION=${DEFAULT_NGINX_INGRESS_TAG}
-elif [ ${MODE} == "nginx-ingress" ]; then
+elif [ "${MODE}" == "nginx-ingress" ]; then
     DEFAULT_LABEL="${DEFAULT_NGINX_INGRESS_REPOSITORY}:${DEFAULT_NGINX_INGRESS_TAG}"
     ALT_REPOSITORY="${ACR_FQDN}/${DEFAULT_NGINX_INGRESS_REPOSITORY}"
     ALT_VERSION="${DEFAULT_TAG}"
