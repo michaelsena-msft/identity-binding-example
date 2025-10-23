64f2189dcfb0b26f837bdbd667ee1eea74219692
Michael Sena <michaelsena@microsoft.com>
2025-10-23 12:07:02 +1100

    Fix step 07

--- 07-replace-image.sh
+++ 07-replace-image.sh
@@ -2,25 +2,27 @@
 set -eou pipefail
 [ -f ./.env ] && . ./.env || . ../.env
 
-if [ "${MODE}" == "ingress-nginx" ]; then
+if [ "${MODE}" = "ingress-nginx" ]; then
     DEFAULT_LABEL=$(grep -o '^-\W\+image:.*$' ./patches/controller-image.patch | awk '{print $3}')
-    ALT_REPOSITORY=$(echo ${DEFAULT_LABEL} | sed -e "s/registry.k8s.io/${ACR_FQDN}/g; s/@.\+//g")
-    ALT_VERSION=${DEFAULT_NGINX_INGRESS_TAG}
-elif [ "${MODE}" == "nginx-ingress" ]; then
+    ALT_LABEL=$(echo ${DEFAULT_LABEL} | sed -e "s/registry.k8s.io/${ACR_FQDN}/g; s/@.\+//g")
+elif [ "${MODE}" = "nginx-ingress" ]; then
     DEFAULT_LABEL="${DEFAULT_NGINX_INGRESS_REPOSITORY}:${DEFAULT_NGINX_INGRESS_TAG}"
-    ALT_REPOSITORY="${ACR_FQDN}/${DEFAULT_NGINX_INGRESS_REPOSITORY}"
-    ALT_VERSION="${DEFAULT_TAG}"
+    ALT_LABEL=${ACR_FQDN}/${DEFAULT_NGINX_INGRESS_REPOSITORY}:${DEFAULT_NGINX_INGRESS_TAG}
 fi
+
+log Pull and tagging image
+info DEFAULT_LABEL:  ${DEFAULT_LABEL}
+info ALT_LABEL:      ${ALT_LABEL}
 
 log Pulling default image ${DEFAULT_LABEL}
 docker pull ${DEFAULT_LABEL}
     
 log Tagging image
-docker tag ${DEFAULT_LABEL} ${ALT_REPOSITORY}:${ALT_VERSION}
+docker tag ${DEFAULT_LABEL} ${ALT_LABEL}
     
 log Pushing
-docker push ${ALT_REPOSITORY}:${ALT_VERSION}
+docker push ${ALT_LABEL}
 
-./operations/configure-${MODE}.sh ${ALT_IMAGE}
+./operations/configure-${MODE}.sh ${ALT_LABEL}
 
 ./operations/verify.sh
